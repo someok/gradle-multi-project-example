09279663dc6bdcf8469d712e7e79fcb57a754ce5
JX Wang <wjianxu@gmail.com>
2017-03-25 10:28:05 +0800

    优化一下示例，去掉一些不必要的东西

--- project-business/src/main/java/com/example/business/UserService.java
+++ project-business/src/main/java/com/example/business/UserService.java
@@ -1,12 +1,10 @@
 package com.example.business;
 
 import com.example.model.User;
-import org.springframework.stereotype.Service;
 
 /**
  * Created by wjx on 15/7/5.
  */
-@Service
 public class UserService {
 
     public User getUser() {
--- project-common/src/main/java/com/example/model/User.java
+++ project-common/src/main/java/com/example/model/User.java
@@ -1,14 +1,10 @@
 package com.example.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
 import java.io.Serializable;
 
 /**
  * Created by wjx on 15/7/5.
  */
-@Entity
-@Table(name = "T_USER")
 public class User implements Serializable {
 
     private String name;
